97f40f2c83b9d9e4ca08ad5ac57e45b075325118
aidanhal22 <90794198+aidanhal22@users.noreply.github.com>
2022-09-26 11:05:00 -0500

    9-26

--- app/src/main/java/com/example/musicmenu/DetailActivity.java
+++ app/src/main/java/com/example/musicmenu/DetailActivity.java
@@ -2,7 +2,10 @@ package com.example.musicmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,37 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        try {
+            // This gets the particular Detailed object we are displaying
+            // In this case, it is a Food object.
+            Music myMusic = intent.getParcelableExtra(CategoryActivity.DETAIL_CHOICE);
+
+            // This page will display the specific Detailed data for what your app is showing.
+            // Get references to the xml views for name, price, desc, and photo
+
+            // Finish this code to match your xml file.  These names are what I used.
+            TextView title = findViewById(R.id.title);
+            TextView artist = findViewById(R.id.artist);
+            TextView desc = findViewById(R.id.descTextView);
+            ImageView photo = findViewById(R.id.photoImageView);
+
+            // set values on the screen based on the object that was passed to this Detail activity
+            title.setText(myMusic.getName());
+            artist.setText("$"+myMusic.getPrice());
+            desc.setText(myFood.getDesc());
+
+            if (myMusic.getImageResourceID() == 0) {
+                // set a default pic or decide what to do in this case.
+            }
+            else {
+                photo.setImageResource(myMusic.getImageResourceID());
+            }
+        }
+        catch (Exception e) {
+            Log.i("Aidan", "Music app not working!");
+        }
     }
+
 }
--- app/src/main/java/com/example/musicmenu/MainActivity.java
+++ app/src/main/java/com/example/musicmenu/MainActivity.java
@@ -2,13 +2,69 @@ package com.example.musicmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    public static final String ARRAYLIST_VALUES = "Data to display in listView";
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // create an OnItemClickListener (notice the O is capitalized!!!)
+        // since the ListView isn't a subclass of Button, that is why we have to implement a listener
+        // and cannot use an onClick attribute like we did for Buttons, checkboxes or radio buttons
+        // we can use this method with array data defined in the strings.xml
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> listView,
+                                    View itemView, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
+
+
+                // create an ArrayList of whatever type of data you are displaying in this app
+                // In this particular app, everything is centralized around the Food class.
+                ArrayList<Music> listData = new ArrayList<>();
+
+                // the choices for the menu on MainActivity coincide with the values in arr in strings.xml file
+                if (position == 0) {                    // they chose breakfast
+                    for (Music f : Music.country) {      // add all the elements for this array to the arraylist
+                        listData.add(f);
+                    }
+                } else if (position == 1) {
+                    for (Music f : Music.hipHop) {        // they chose lunch
+                        listData.add(f);
+                    }
+                } else if (position == 2) {
+                    for (Music f : Music.pop) {        // they chose dinner
+                        listData.add(f);
+                    }
+                }
+
+                // Send this particular ArrayList of Food data to the next activity, where we display
+                // each name of each object in the ArrayList.
+                intent.putParcelableArrayListExtra(ARRAYLIST_VALUES, listData);
+                startActivity(intent);
+
+            }
+        };
+//Get a reference to the ListView in the XML
+        ListView listView = (ListView) findViewById(R.id.list_options);
+
+
+//Add the listener to the ListView
+// without this line of code, the listView isn’t interactive!!!!
+        listView.setOnItemClickListener(itemClickListener);
+
     }
+
+
 }
